aeb69023c7bde5fcb39fd319ce242f79b10504d9
Elizeu Louzeiro <elizeumatheus16@gmail.com>
2020-10-23 20:55:42 -0300

    feat: moved global styles to folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Routes from 'routes'
-import 'styles/index.scss'
+import 'styles/global/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
