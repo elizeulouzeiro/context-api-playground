4c3575d89195187742d1c9616f2ae94603a0f2fe
Elizeu Louzeiro <elizeumatheus16@gmail.com>
2020-10-24 00:22:19 -0300

    feat: created a list of languages

---
+++ src/pages/Repositories/utils/languages.ts
@@ -0,0 +1,52 @@
+export default [
+  {
+    value: 'python',
+    name: 'Python',
+    color: '#3573a5'
+  },
+  {
+    value: 'javascript',
+    name: 'JavaScript',
+    color: '#f2e159'
+  },
+  {
+    value: 'java',
+    name: 'Java',
+    color: '#b07316'
+  },
+  {
+    value: 'c#',
+    name: 'C#',
+    color: '#198601'
+  },
+  {
+    value: 'c',
+    name: 'C',
+    color: '#555555'
+  },
+  {
+    value: 'c++',
+    name: 'C++',
+    color: '#f34c7c'
+  },
+  {
+    value: 'go',
+    name: 'Go',
+    color: '#05acd9'
+  },
+  {
+    value: 'r',
+    name: 'R',
+    color: '#1b8ce7'
+  },
+  {
+    value: 'swift',
+    name: 'Swift',
+    color: '#ffad46'
+  },
+  {
+    value: 'php',
+    name: 'PHP',
+    color: '#4f5f95'
+  }
+]
